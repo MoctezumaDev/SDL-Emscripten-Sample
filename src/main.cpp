d50ec3568061d824a158a2928d26e77dc4b4dcab
moctezuma.dev <leon.moctezuma@gmail.com>
2025-11-21 16:56:34 +0100

    Clean up logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdexcept>
+#include <string>
 #include <fmt/color.h>
 #include <fmt/core.h>
 #include <SDL3/SDL.h>
@@ -40,20 +42,8 @@ void main_loop(void) {
     }
 }
 
-int main(int argc, char** argv)
+void main_start_loop()
 {
-    // Set the hint BEFORE window creation or SDL_Init
-    SDL_SetHint(SDL_HINT_EMSCRIPTEN_KEYBOARD_ELEMENT, "#canvas");
-
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return -1;
-    }
-    SDL_Log("Success");
-
-    window = SDL_CreateWindow("App", 640, 480, SDL_WINDOW_RESIZABLE);
-    renderer = SDL_CreateRenderer(window, nullptr);
-
 #ifdef __EMSCRIPTEN__
     // The browser controls the loop now
     emscripten_set_main_loop(main_loop, 0, 1);
@@ -64,9 +54,43 @@ int main(int argc, char** argv)
         SDL_Delay(16);
     }
 #endif
+}
 
+void main_finalize()
+{
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+void main_initialize()
+{
+    // Set the hint BEFORE window creation or SDL_Init
+    SDL_SetHint(SDL_HINT_EMSCRIPTEN_KEYBOARD_ELEMENT, "#canvas");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        throw std::runtime_error(
+            std::string("SDL_Init failed: ") + SDL_GetError()
+        );
+    }
+    SDL_Log("Success");
+
+    window = SDL_CreateWindow("App", 640, 480, SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, nullptr);
+}
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        main_initialize();
+        main_start_loop();
+        main_finalize();
+    }
+    catch (const std::exception& e) {
+        std::cerr <<  e.what() << "\n";
+        return 1;
+    }
+    
     return 0;
 }
